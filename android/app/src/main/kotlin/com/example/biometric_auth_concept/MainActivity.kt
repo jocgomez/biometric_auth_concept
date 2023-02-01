723b3d17c859c0b991e204fe1be046a265a285d9
jocgomez <dev.jocgomez@gmail.com>
2023-02-01 11:14:21 -0500

    Implementation. 2. Android integration

--- android/app/src/main/kotlin/com/example/biometric_auth_concept/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biometric_auth_concept/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.biometric_auth_concept
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
